5420b62e7ba7c148dc52eb5db7108af253d39f61
asp <asp@aptech.com>
2024-11-26 13:02:26 +0500

    Crud Operations Completed

--- api-integration/src/app/app.routes.ts
+++ api-integration/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { EventsComponent } from './events/events.component';
 import { AddEventComponent } from './add-event/add-event.component';
+import { EditEventComponent } from './edit-event/edit-event.component';
 
 export const routes: Routes = [
 
@@ -12,6 +13,12 @@ export const routes: Routes = [
     {
         path:"addEvent",
         component:AddEventComponent,
+        
+    },
+    
+    {
+        path:"editEvent/:id",
+        component:EditEventComponent,
         
     }
 ];
--- api-integration/src/app/events/events.component.ts
+++ api-integration/src/app/events/events.component.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [],
+  imports: [`RouterLink`],
   templateUrl: './events.component.html',
   styleUrl: './events.component.css'
 })
